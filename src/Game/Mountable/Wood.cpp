c6d3554d9f15fb38590a729d309b3bc7399db78c
TrangDangDucTin <115961028+trangdangductin@users.noreply.github.com>
2024-01-09 12:04:18 +0700

    Delete Wood

--- src/Game/Mountable/Wood.cpp
+++
@@ -1,6 +0,0 @@
-#include "Wood.h"
-
-Wood::Wood() {
-    setSize(sf::Vector2f(constant::WOOD_WIDTH, constant::WOOD_HEIGHT));
-    setTexture(TextureHolder::getInstance()->getTexture((int)Textures::Mountable::Wood));
-}
